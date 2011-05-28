3c2b8bfe0a2ba5cc3d01fcde4cb7544da2b9797d
Nimalan Mahendran <nmahendran@alumni.uwaterloo.ca>
2011-05-28 04:41:09 -0700

    Made URL resolver error handling more robust

--- urlresolver.py
+++ urlresolver.py
@@ -36,26 +36,53 @@ class URLResolver(object):
         self.resolved_urls.insert(new_entry)
 
         return url, False
+
+    def handle_failed_resolve(self, err, url, code=None):
+        if code is None:
+            code = err.code
+
+        if code == 404 or code == 403: # resource not found/forbidden
+            logging.info("Failed to resolve %s: Not found" % (url))
+            self.set_url_as_resolved(url, url)
+        elif code == 408 or code == 503: # request timeout
+            logging.info("Failed to resolve %s: Timeout" % (url))
+            
+            query = {"url" : url, "resolved" : False}
+            update = {"$inc" : {"timeouts" : 1}}
+            self.resolved_urls.update(query, update)
+        else:
+            raise err
         
     def resolve_url(self, url):
         try:
             resolved_url = urllib2.urlopen(url, None, 1).geturl()
-        except urllib2.URLError as e:
-            logging.info("Failed to resolve %s: %s" % (url, e.reason))
+        except urllib2.HTTPError as e:
+            self.handle_failed_resolve(e, url)
             return None
+        except urllib2.URLError as e:
+            if str(e) == "<urlopen error timed out>":
+                self.handle_failed_resolve(e, url, 408)
+                return None
+            else:
+                raise e
 
-        criteria = {"url" : url, "resolved" : False}
+        self.set_url_as_resolved(url, resolved_url)
+        return resolved_url
+
+    def set_url_as_resolved(self, url, resolved_url):
+        query = {"url" : url, "resolved" : False}
         update = {"$set" : {"resolved_url" : resolved_url,
                             "resolved" : True}}
         
-        self.resolved_urls.update(new_entry)
+        self.resolved_urls.update(query, update)
         return resolved_url
 
     def resolve_unresolved_urls(self, cbs=[]):
+        sort = [("timeouts", 1)]
         query = {"resolved" : False}
 
         while self.running:
-            result = self.resolved_urls.find_one(query)
+            result = self.resolved_urls.find_one(query, sort=sort)
 
             if result is None:
                 time.sleep(10)
