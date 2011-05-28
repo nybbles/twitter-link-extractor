03daede045357309ff852860030f5eaa0dd88ea3
Nimalan Mahendran <nmahendran@alumni.uwaterloo.ca>
2011-05-27 18:08:37 -0700

    Link extractor connected to twitter and tracking "vancouver"
    - Using tweepy library instead of twitter-stream.
    - Using OAuth with single access token instead of basic auth.

--- link-extractor.py
+++ link-extractor.py
@@ -1,7 +1,43 @@
 #!/usr/bin/env python
-twitter_stream_lib_loc = ""
+tweepy_lib_loc = ""
 
 import sys
-sys.path.append(twitter_stream_lib_loc)
+sys.path.append(tweepy_lib_loc)
 
-from twitter.stream import BaseStreamingHandler
+import tweepy as twpy
+from tweepy.error import TweepError
+
+class TwitterLinkExtractor(twpy.streaming.StreamListener):
+    def on_error(self, status_code):
+        err_str = "Error! %d" % (status_code)
+        raise TweepError(err_str)
+    
+    def on_timeout(self):
+        raise TweepError("Timeout!")
+
+    def on_status(self, status):
+        self.extract_link(status)
+        print "%s at %s: %s" % \
+              tuple(map(lambda x: x.encode('latin-1'),
+                        (status.author.screen_name,
+                         str(status.created_at), status.text)))
+
+    def extract_link(self, status):
+        extract_link(status.text)
+
+def extract_link(text):
+    pass
+
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+twitterauth = \
+    twpy.auth.OAuthHandler(consumer_key, consumer_secret, secure=True)
+twitterauth.set_access_token(access_token, access_token_secret)
+
+twitterlistener = TwitterLinkExtractor()
+twitterstream = twpy.streaming.Stream(twitterauth, twitterlistener)
+
+twitterstream.filter(track=["vancouver"])
